89da001dd0e43c2758cbfa4d4536ec068a86a244
shubham dixit <shubhamdixit863@gmail.com>
2021-10-08 20:52:11 +0530

    fixed issue

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,10 @@ import React from 'react'
 import {useSelector} from "react-redux";
 
 function Header() {
-    //const count = useSelector((state) => state.counter)
+    const count = useSelector((state) => state.counter)
     return (
         <div>
-            
+            {count.value}
             
         </div>
     )
